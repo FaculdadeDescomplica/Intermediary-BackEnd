049454818120732c00984272cd9ba5b79116471a
Ana Teixeira <acrispteixeira@gmail.com>
2023-03-13 14:49:35 -0300

    Ajuste index para módulo 5

--- HERO CLOUD - PROJETO/lgpd-back/index.js
+++ HERO CLOUD - PROJETO/lgpd-back/index.js
@@ -2,13 +2,27 @@ import express from "express";
 import pkg from 'body-parser';
 import router from "./routes/routes.js";
 
+import sequelize from "./utils/database.js";
+import association from "./models/Associations.js";
+
 const app = express();
 const { json, urlencoded } = pkg;
+
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use("/", router);
 
-app.listen(3000, function(){
-    console.log("Listening to port 3000");
-})
+(async () => {
+    try {
+        association.associations();
+        await sequelize.sync();
+        app.listen(3000, function () {
+            console.log("Listening to port 3000");
+        });
+    } catch (err) {
+        console.log(err);
+    }
+})();
+
+app.use("/", router);
