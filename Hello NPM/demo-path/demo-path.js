0f9730715085e366073832bbcf7919667fff45d0
Ana Teixeira <acrispteixeira@gmail.com>
2023-03-12 22:34:04 -0300

    Ajuste para demonstração de módulos do node

--- Hello NPM/demo-path/demo-path.js
+++ Hello NPM/demo-path/demo-path.js
@@ -1,4 +1,13 @@
-import { basename } from 'path';
+import { basename, dirname } from 'path';
 
-let filename = basename('./teste.txt');
-console.log(filename);
+let nome_do_arquivo = basename('./teste.txt');
+let filename = basename('/test/something');
+
+let dir = dirname('/test/something') // /test
+let diretorio = dirname('/test/something/file.txt') // /test/something
+
+console.log("nome do arquivo 1 = " + nome_do_arquivo);
+console.log("nome do arquivo 2 = " + filename);
+
+console.log("diretorio 1 = " + dir);
+console.log("diretorio 2 = " + diretorio);
