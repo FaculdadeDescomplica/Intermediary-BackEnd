f899c67cbc892f52ac60249ae883dfe2bd9548a5
Ana Teixeira <acrispteixeira@gmail.com>
2023-03-13 00:39:05 -0300

    Adicionando primeira versão script de banco

---
+++ HERO CLOUD - PROJETO/Script - lgpd-database.sql
@@ -0,0 +1,45 @@
+drop table "Users";
+drop table "Teachers" ;
+drop table "Courses" ;
+drop table "Evaluations" ;
+
+CREATE TABLE "Users" (
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR ( 50 ) NOT NULL,
+	last_name VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) NOT NULL,
+	gender VARCHAR(55) NOT NULL
+);
+
+create table "Courses" (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL
+);
+
+create table "Teachers" (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR ( 50 ) NOT NULL,
+	course_id INTEGER NOT null,
+	CONSTRAINT fk_courses
+      FOREIGN KEY(course_id) 
+	  REFERENCES "Courses"(id)
+);
+
+
+create table "Evaluations" (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER NOT NULL,
+	concept VARCHAR (50) NOT null,
+	course_id INTEGER NOT null,
+	CONSTRAINT fk_courses
+      FOREIGN KEY(course_id) 
+	  REFERENCES "Courses"(id),
+	CONSTRAINT fk_users
+      FOREIGN KEY(user_id) 
+	  REFERENCES "Users"(id)
+);
+
+select * from "Users";
+select * from "Teachers";
+select * from "Evaluations";
+select * from "Courses";
